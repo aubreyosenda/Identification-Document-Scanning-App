a816851912d81b283a9778fd0183f1ce0ce8d34c
ndaruga <ndaruga.francis@gmail.com>
2024-07-11 15:38:41 +0300

    extract name and ID no from card

--- app/src/main/java/com/android/example/cameraxapp/DisplayActivity.java
+++ app/src/main/java/com/android/example/cameraxapp/DisplayActivity.java
@@ -25,7 +25,7 @@ public class DisplayActivity extends AppCompatActivity {
 
 
     private ImageView imageView;
-    private TextView textView;
+    private TextView textNameView, textIdNoView, textPhoneNoView;
 
 
     @Override
@@ -34,7 +34,9 @@ public class DisplayActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_display);
         imageView = findViewById(R.id.imageView);
-        textView = findViewById(R.id.text);
+        textNameView = findViewById(R.id.name_text);
+        textIdNoView = findViewById(R.id.id_no_text);
+        textPhoneNoView = findViewById(R.id.phone_number_text);
 
 
         // Get the image URI passed from MainActivity1
@@ -74,15 +76,51 @@ public class DisplayActivity extends AppCompatActivity {
                 stringBuilder.append(textBlock.getValue());
                 stringBuilder.append("\n");
             }
+
+//            Get all text from stringBuilder
+            String allText = stringBuilder.toString();
+            String[] lines = allText.split("\n");  // Create a new array to store modified strings
+
+            // remove all white spaces
+            for (int i = 0; i < lines.length; i++) {
+                lines[i] = lines[i].replaceAll("\\s", "");
+            }
+
+            textNameView.setText(getFullName(lines[lines.length-1])); // Last line in the scan
+            textIdNoView.setText(getIDNo(lines[lines.length-2])); // Second last line in the scan
 //            Log.d("Info", "Text begins here" + stringBuilder.toString());
-            textView.setText(stringBuilder.toString());
 
-            // Make the TextView editable
-            textView.setFocusable(true);
-            textView.setFocusableInTouchMode(true);
-            textView.setClickable(true);
-            textView.setCursorVisible(true); // Show the cursor
+//            textView.setText(stringBuilder.toString());
 
+//            // Make the TextView editable
+//            textView.setFocusable(true);
+//            textView.setFocusableInTouchMode(true);
+//            textView.setClickable(true);
+//            textView.setCursorVisible(true); // Show the cursor
+
+        }
+    }
+
+    public static String reverseString(String text) {
+        return new StringBuilder(text).reverse().toString();
+    }
+
+    public static String getFullName(String newText){
+        // System.out.println(newText);
+        return newText.replaceAll("<", " ");
+    }
+
+    public static String getIDNo(String text) {
+        String reversedText = reverseString(text);
+        String reversedIdNo = reversedText.substring(4, 13);
+        String dummyIdNo = reverseString(reversedIdNo);
+
+        // Check if dummyIdNo starts with '0' or 'O'
+        if (dummyIdNo.startsWith("0") || dummyIdNo.startsWith("O") || dummyIdNo.startsWith("o")) {
+            // Discard the first character
+            return dummyIdNo.substring(1);
+        } else{
+            return dummyIdNo;
         }
     }
 }
