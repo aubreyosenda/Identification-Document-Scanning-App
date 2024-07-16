d521c732dcf78bbc03937f6862622ed46985c884
ndaruga <ndaruga.francis@gmail.com>
2024-07-17 01:22:54 +0300

    extract details from passport

--- app/src/main/java/com/android/example/cameraxapp/GetStartedActivity.java
+++ app/src/main/java/com/android/example/cameraxapp/GetStartedActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.hbb20.CountryCodePicker;
 
@@ -23,6 +25,11 @@ public class GetStartedActivity extends AppCompatActivity {
         countryCodePicker = findViewById(R.id.ccp);
         radioGroup = findViewById(R.id.radio_group);
         scanButton = findViewById(R.id.button_add);
+//        Buttons
+        RadioButton radio_id_card = findViewById(R.id.radio_id_card);
+        RadioButton radio_passport = findViewById(R.id.radio_passport);
+        RadioButton radio_driving_license = findViewById(R.id.radio_driving_license);
+
 
         // Set default country (optional, as it's set in XML)
         countryCodePicker.setDefaultCountryUsingNameCode("KE");
@@ -33,16 +40,20 @@ public class GetStartedActivity extends AppCompatActivity {
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int selectedId = radioGroup.getCheckedRadioButtonId();
-                RadioButton selectedRadioButton = findViewById(selectedId);
-                String selectedDocument = selectedRadioButton.getText().toString();
-                String selectedCountry = countryCodePicker.getSelectedCountryName();
+                if (!radio_id_card.isChecked() && !radio_passport.isChecked() && !radio_driving_license.isChecked()){
+                    Toast.makeText(GetStartedActivity.this, "Please select a document", Toast.LENGTH_SHORT).show();
+                } else{
+                    int selectedId = radioGroup.getCheckedRadioButtonId();
+                    RadioButton selectedRadioButton = findViewById(selectedId);
+                    String selectedDocument = selectedRadioButton.getText().toString();
+                    String selectedCountry = countryCodePicker.getSelectedCountryName();
 
-                // Start MainActivity1 and pass selected document type
-                Intent intent = new Intent(GetStartedActivity.this, CameraActivity.class);
-                intent.putExtra("selectedDocument", selectedDocument);
-                intent.putExtra("selectedCountry", selectedCountry);
-                startActivity(intent);
+                    // Start MainActivity1 and pass selected document type
+                    Intent intent = new Intent(GetStartedActivity.this, CameraActivity.class);
+                    intent.putExtra("selectedDocument", selectedDocument);
+                    intent.putExtra("selectedCountry", selectedCountry);
+                    startActivity(intent);
+                }
             }
         });
     }
